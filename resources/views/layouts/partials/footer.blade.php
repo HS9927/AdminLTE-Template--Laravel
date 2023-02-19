dbcfbf2bb8ec7529c21bf8fabe1813670807841e
Snoopy Devv <snoopydevv@gmail.com>
2023-02-19 14:59:00 +0700

    skyfall

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,8 @@
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+
+    <div class="float-right d-none d-sm-inline-block">
+        Code Dev Dev (Snoopy Dev)
+    </div>
+  </footer>
---
+++ resources/views/layouts/partials/nav.blade.php
@@ -0,0 +1,13 @@
+<!-- Navbar -->
+<nav class="main-header navbar navbar-expand navbar-dark">
+
+    {{-- Left Header --}}
+    @include("layouts.partials.left-header")
+    {{-- Left Header --}}
+
+    {{-- Right Header --}}
+    @include("layouts.partials.right-header")
+    {{-- Right Header --}}
+
+</nav>
+<!-- /.navbar -->
